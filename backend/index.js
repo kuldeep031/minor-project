1650bfe9e1993d0a2610421e2b11c51b998323d9
Kuldeep <kuldeep@gmail.com>
2025-05-19 01:30:27 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -62,7 +62,3 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-// ✅ Optional: Base route
-app.get("/", (req, res) => {
-  res.send("Minor Project Management System API is running...");
-});
